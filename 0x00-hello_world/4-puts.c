4bbfd25c28ac26c925c27e820982b3d30766d81c
Onyedikachi29 <dicksongodwin0@gmail.com>
2023-03-17 02:53:54 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ * main - Programming is like building a multilingual puzzle, followed by a new line
  *
  * Return: always 0
  */
 
-int main(void)
 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }
