6694c35da57affe6322a543cc40ebfed25f18216
Onyedikachi29 <dicksongodwin0@gmail.com>
2023-03-18 13:02:25 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,4 +8,19 @@
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar('.');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
 
