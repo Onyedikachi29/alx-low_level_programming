77f5fbf37a2a2fb952e3aba50cac0a5dc85f207c
Onyedikachi29 <dicksongodwin0@gmail.com>
2023-03-18 04:05:07 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - I play what I call the alphabet game
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
