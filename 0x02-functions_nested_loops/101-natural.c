c2430746cd168f71f783e50079708c8e5b28bf3b
Onyedikachi29 <dicksongodwin0@gmail.com>
2023-03-23 07:05:58 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int i, z = 4;
+
+	while (i < 1024)
+	{
+	if ((i % == 0) || (i % 5 == 0))
+	{
+	z += 15
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
