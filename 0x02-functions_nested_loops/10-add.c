f14ed6b36679f10caf11580ad54af64272e9ed5d
Onyedikachi29 <dicksongodwin0@gmail.com>
2023-03-23 04:00:13 -0700

    Add a and b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,14 @@
 
 /**
 * add - Adds two values
-* n@1: The first number to be computed
-* n@2: The second number to be computed
+* @n1: The first number to be computed
+* @n2: The second number to be computed
 * Return: Integer value
 */
 
 int add(int n1, int n2)
 
 {
-	return (int 1 + int 2);
+	return (n1 + n2);
 
 }
