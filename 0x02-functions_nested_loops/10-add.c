21ecee412079954446c99faece0e73b275d86aec
Onyedikachi29 <dicksongodwin0@gmail.com>
2023-03-23 03:29:16 -0700

    Task 8

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-*  add - Adds two values
-*  n@1: The first number to be computed
-*  n@2: The second number to be computed
-*  Return:  Integer value
+* add - Adds two values
+* n@1: The first number to be computed
+* n@2: The second number to be computed
+* Return: Integer value
 */
 
 int add(int n1, int n2)
 
 {
-
-return (int 1 + int 2);
+	return (int 1 + int 2);
 
 }
